de307b792f4cf740442f2297a680695599ff5b47
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2025-10-09 17:05:03 +0200

    chore: lint and format

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Cog, FileUp, MessagesSquare } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
